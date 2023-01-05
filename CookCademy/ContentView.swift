cecd65b9793cb1574fd78e33a074b6fa20da2913
plimacarlos <plimacarlos@brq.com>
2023-01-05 17:52:26 -0300

    Initial Commit

---
+++ CookCademy/ContentView.swift
@@ -0,0 +1,21 @@
+////  ContentView.swift
+//  CookCademy
+//
+//  Created by Patricia Carlos on 05/01/23.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookCademy/CookCademyApp.swift
@@ -0,0 +1,17 @@
+////  CookCademyApp.swift
+//  CookCademy
+//
+//  Created by Patricia Carlos on 05/01/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct CookCademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
