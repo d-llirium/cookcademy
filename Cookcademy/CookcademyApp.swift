d81868194a5dfcba67b800a60584692302591430
plimacarlos <plimacarlos@brq.com>
2023-06-07 17:51:09 -0300

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+////  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Patricia Carlos on 07/06/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
