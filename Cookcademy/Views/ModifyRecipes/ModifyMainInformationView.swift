6eb43a23219cb67d2cbebc3327010c64800c8491
User <user@MB033361.local>
2023-11-09 13:24:54 -0300

    add Color for Add New Recipe

--- Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyMainInformationView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ModifyMainInformationView: View {
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     @Binding var mainInformation: MainInformation // is wrapped with @Binding which means this view does not own it.
     
     var body: some View {
         Form {
             TextField("Recipe Name", text: $mainInformation.name)
+                .listRowBackground(listBackgroundColor)
             TextField("Author", text: $mainInformation.author)
+                .listRowBackground(listBackgroundColor)
             Section(
                 header: Text("Description")
             ) {
                 TextEditor(text: $mainInformation.description)
+                    .listRowBackground(listBackgroundColor)
             }
             Picker(selection:  $mainInformation.category,label:
                     HStack {
@@ -30,8 +36,10 @@ struct ModifyMainInformationView: View {
                     Text(category.rawValue)
                 }
             }
+            .listRowBackground(listBackgroundColor)
             .pickerStyle(MenuPickerStyle())
         }
+        .foregroundColor(listTextColor)
     }
 }
 
